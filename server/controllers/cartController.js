8b06995da12b7f62a69459773f7d59fa3dc07c56
WillHoll <willhollingsworth15@yahoo.com>
2019-11-04 16:12:42 -0700

    step 9 complete

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -2,13 +2,32 @@ const swag = require('../models/swag');
 
 module.exports = {
     add: (req, res) => {
-        const {id} = req.params;
-        if (req.session.user.cart.find)
+        const { user } = req.session;
+        const { id } = req.params;
+        if (user.cart.find(swag => swag.id === Number(id))) {
+            res.status(200).send(user)
+        } else {
+            const product = swag.find(item => item.id === Number(id))
+            user.cart.push(product)
+            user.total += product.price;
+            res.status(200).send(user)
+        }
     },
     delete: (req, res) => {
-
+        const { user } = req.session;
+        const { id } = req.params;
+        const removedObj = user.cart.find(el => el.id === Number(id));
+        const index = user.cart.findIndex(el => el.id === Number(id));
+        if (index !== -1) {
+            user.total -= removedObj.price;
+            user.cart.splice(index, 1);
+        }
+        res.status(200).send(user)
     },
     checkout: (req, res) => {
-
+        const { user } = req.session;
+        user.cart = [];
+        user.total = 0;
+        res.status(200).send(user)
     }
 }
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const checkForSession = require('./middlewares/checkForSession');
 const swagController = require('./controllers/swagController');
 const authController = require('./controllers/authController');
+const cartController = require('./controllers/cartController');
 const {SESSION_SECRET, SERVER_PORT } = process.env;
 const app = express();
 
@@ -21,5 +22,9 @@ app.post('/api/register', authController.register)
 app.post('/api/signout', authController.signout)
 //swag endpoints
 app.get('/api/swag', swagController.read)
+//cart endpoints
+app.post('/api/cart/checkout', cartController.checkout)
+app.post('/api/cart/:id', cartController.add)
+app.delete('/api/cart/:id', cartController.delete)
 
 app.listen(SERVER_PORT, () => console.log(`I would like ${SERVER_PORT} weed please.`));
